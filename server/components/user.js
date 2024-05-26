8fbe1d765fbe2f092d8819059ecbf52abfa8aa7e
Manuraj <mpv799@gmail.com>
2024-05-26 20:02:54 +0530

    added routes for veiwing users and added middleware logic for verifying user using JWT token

--- server/components/user.js
+++ server/components/user.js
@@ -1,6 +1,8 @@
-var bcrypt = require('bcrypt');
-var validator = require('../lib/validation')
-var model = require('../models/schema')
+var bcrypt = require("bcrypt");
+var validator = require("../lib/validation");
+var model = require("../models/schema");
+var apiAuth = require("../lib/middleware");
+var logger = require("../lib/logger");
 
 exports.userReg = async (req, res) => {
   try {
@@ -37,9 +39,81 @@ exports.userReg = async (req, res) => {
       }
     }
   } catch (err) {
-    // logger.error(
-    //   `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
-    // );
+    logger.error(
+      `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
+    );
+    res.status(err.status || 500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.userLogin = async (req, res) => {
+  try {
+    //Checking email Id exist in DB
+    const user = await model.User.findOne({
+      emailId: req.body.emailId,
+    });
+    if (!user) {
+      var err = new Error("Invalid email Id or Password !");
+      err.status = 401;
+      throw err;
+    }
+
+    //validating password using bcrypt
+    const validCred = await bcrypt.compare(req.body.password, user.password);
+    if (!validCred) {
+      var err = new Error("Invalid email Id or Password* !");
+      err.status = 401;
+      throw err;
+    } else {
+      const accessToken = apiAuth.generateAccessToken(req.body.emailId);
+      res.status(200).json({
+        status: "Success",
+        message: "User Login Success",
+        userId: user.id,
+        emailId: user.emailId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        accessToken,
+      });
+    }
+  } catch (err) {
+    logger.error(
+      `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message} ${err.stack}`
+    );
+    res.status(err.status || 500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.viewUser = async (req, res) => {
+  try {
+    //check if the login user is same as the requested user
+    apiAuth.validateUser(req.user, req.body.emailId);
+    const user = await model.User.findOne(
+      {
+        emailId: req.body.emailId,
+      },
+      {
+        //set password 0 so that password is not sent to client
+        password: 0,
+      }
+    );
+    if (!user) {
+      var err = new Error("User does not exist!");
+      err.status = 400;
+      throw err;
+    }
+    res.status(200).json({
+      status: "Success",
+      user: user,
+    });
+  } catch (err) {
+    logger.error(
+      `URL : ${req.originalUrl} | staus : ${err.status} | message: ${err.message}`
+    );
     res.status(err.status || 500).json({
       message: err.message,
     });
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var controller = require('../components/user');
-
+var middleware = require('../lib/middleware');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -9,5 +9,11 @@ router.get('/', function (req, res, next) {
 
 //User Registeration router
 router.post('/v1/register', controller.userReg)
+
+//User Login router
+router.post('/v1/login', controller.userLogin)
+
+//view User
+router.post('/v1/view',middleware.validateToken,controller.viewUser)
 
 module.exports = router;
